21f16cb511c1980c069f38eabb318f1241dc61f5
smartbison <121305911+smartbison@users.noreply.github.com>
2025-01-15 20:00:27 -0500

    merging

--- app/about/council/page.tsx
+++ app/about/council/page.tsx
@@ -7,15 +7,15 @@ const rishi_bio = "/council_bio/rs.webp";
 
 export default function Council() {
     return (
-        <div className = "flex w-screen h-screen">
+        <div className = "flex w-screen">
             <div className = "flex flex-col w-full justify-center items-center">
                     <h2>The Council</h2>
                     <p >Meet the council members running the show behind the curtains!</p>
                     
                     <div>
-                        <Image src={ishan_bio} alt="Bio for Council Member Ishan" width={500} height={500}/>
-                        <Image src={salman_bio} alt="Bio for Council Member Salman" width={500} height={500} />
-                        <Image src={rishi_bio} alt="Bio for Council Member Rishi" width={500} height={500} />
+                        <img src={ishan_bio} className="w-full" alt="Bio for Council Member Ishan"/>
+                        <img src={salman_bio} alt="Bio for Council Member Salman" />
+                        <img src={rishi_bio} alt="Bio for Council Member Rishi"/>
                     </div>
             
                     <p>And you too! There is no BOUNC without our players and members.</p>
