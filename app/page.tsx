2446d75b55ea7e68c1a6b2a06024a8e62dd3160e
smartbison <121305911+smartbison@users.noreply.github.com>
2025-01-15 20:32:39 -0500

    header fixed

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <main className="flex flex-col min-w-full w-screen justify-center bg-divprimary">
       <div className = "flex flex-row justify-between items-center header sticky top-0 flex flex-row bg-gradient-to-r from-divprimary to-divsecondary p-2 z-10">
 
-      <div className="flex flex-row items-center gap-2 ">
+      <div className="flex flex-row items-center gap-2 basis-1 grow">
         <a href="https://www.facebook.com/profile.php?id=61564215452432"><Image className = "w-10 hover:scale-105" src="/icons/icons8-facebook.svg" width={50} height={50} alt="facebook icon"/></a>
         <a href="https://www.instagram.com/cricketbounc/"><Image className = "w-8 hover:scale-105" src="/icons/icons8-instagram.svg" width={50} height={50} alt="facebook icon"/></a>
         <a href="https://www.youtube.com/channel/UCY_pRAyhPu98hcduzQl6fqw"><Image className = "w-10 hover:scale-105" src="/icons/icons8-youtube.svg" width={50} height={50} alt="facebook icon"/></a>
@@ -24,7 +24,7 @@ export default function Home() {
       <Image src="/logo.png" alt="Cricket logo" width={50} height={50} />
 
       {/* links */}
-      <div className ="flex gap-4">
+      <div className ="flex gap-4 basis-1 grow justify-end">
       <a className ="font-cricketfont">Home</a>
       <a>About</a>
       <a>Resources</a>
