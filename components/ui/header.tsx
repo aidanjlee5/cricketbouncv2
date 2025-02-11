2c55506a6b64bb9ad88a91d9f16840d8481c218c
smartbison <121305911+smartbison@users.noreply.github.com>
2025-02-10 19:34:30 -0500

    link functionality

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -4,7 +4,6 @@ import { getLayoutOrPageModule } from "next/dist/server/lib/app-dir-module";
 export default function Header(){
   return (
     <div className = "flex flex-row justify-between items-center header sticky top-0 flex flex-row bg-gradient-to-r from-divprimary to-divsecondary p-2 z-10">
-    
       <div className="flex flex-row items-center gap-2 basis-1 grow">
         <a href="https://www.facebook.com/profile.php?id=61564215452432"><Image className = "w-10 hover:scale-105" src="/icons/icons8-facebook.svg" width={50} height={50} alt="facebook icon"/></a>
         <a href="https://www.instagram.com/cricketbounc/"><Image className = "w-8 hover:scale-105" src="/icons/icons8-instagram.svg" width={50} height={50} alt="facebook icon"/></a>
@@ -17,7 +16,7 @@ export default function Header(){
 
       {/* links */}
       <div className ="flex gap-4 basis-1 grow justify-end">
-      <a className ="font-cricketfont">Home</a>
+      <a href="/" className ="font-cricketfont">Home</a>
       <a href="./about/council">About</a>
       <a>Resources</a>
       <a>Socials</a>
